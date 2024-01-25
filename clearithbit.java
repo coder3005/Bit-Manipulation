3278e3840407b9ceef89f96085b40176e93f830d
ABHI GUPTA <138576028+coder3005@users.noreply.github.com>
2024-01-26 00:15:31 +0530

    Create clearithbit.java

---
+++ clearithbit.java
@@ -0,0 +1,11 @@
+package BitManipulation;
+
+public class clearithbit { 
+    public static int clearIthBit(int n, int i) {
+        int bitMask=~(1<<i);
+        return n&bitMask;
+        }
+    public static void main(String[] args) {
+        System.out.println(clearIthBit(10,1));
+    }
+}
