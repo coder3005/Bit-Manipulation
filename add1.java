4061a8baf3764f59c109991c339e6b5c620739c5
ABHI GUPTA <138576028+coder3005@users.noreply.github.com>
2024-02-06 22:29:34 +0530

    Create add1.java

---
+++ add1.java
@@ -0,0 +1,7 @@
+//add 1 to an integer try using bitwise NOT operator
+public class add1 {
+    public static void main(String[] args) {
+        int x=10;
+        System.out.println(-(~x));
+    }
+}
